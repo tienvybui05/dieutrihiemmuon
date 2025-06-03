e921b2c73aa1f1fe776f12940cfaf130ef3a163f
tienvybui05 <tienvy27052005@gmail.com>
2025-06-03 21:40:32 +0700

    source

---
+++ webdieutrihiemmuon/src/main/java/uth/edu/webdieutrihiemmuon/WebdieutrihiemmuonApplication.java
@@ -0,0 +1,13 @@
+package uth.edu.webdieutrihiemmuon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdieutrihiemmuonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebdieutrihiemmuonApplication.class, args);
+    }
+
+}
---
+++ webdieutrihiemmuon/src/test/java/uth/edu/webdieutrihiemmuon/WebdieutrihiemmuonApplicationTests.java
@@ -0,0 +1,13 @@
+package uth.edu.webdieutrihiemmuon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdieutrihiemmuonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

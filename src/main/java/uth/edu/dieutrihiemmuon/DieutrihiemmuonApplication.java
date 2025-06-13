92d955b7aecdce5aadd9580ba711ad9fd824c66f
tienvybui05 <tienvy27052005@gmail.com>
2025-06-13 23:25:55 +0700

    Tạo lại project từ đầu

---
+++ src/main/java/uth/edu/dieutrihiemmuon/DieutrihiemmuonApplication.java
@@ -0,0 +1,13 @@
+package uth.edu.dieutrihiemmuon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieutrihiemmuonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DieutrihiemmuonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uth/edu/dieutrihiemmuon/DieutrihiemmuonApplicationTests.java
@@ -0,0 +1,13 @@
+package uth.edu.dieutrihiemmuon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DieutrihiemmuonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

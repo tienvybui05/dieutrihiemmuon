f12a6f99a864d183d44e9f266582be76680fd163
tienvybui05 <tienvy27052005@gmail.com>
2025-06-14 15:19:16 +0700

    add layout header, footer
    Signed-off-by: tienvybui05 <tienvy27052005@gmail.com>

---
+++ src/main/java/uth/edu/dieutrihiemmuon/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package uth.edu.dieutrihiemmuon.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "customer/index";  // đúng đường dẫn tới file
+    }
+
+}

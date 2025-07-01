e8d1900d876b94bd1497c79676ffc42e8d640ffc
tienvybui05 <tienvy27052005@gmail.com>
2025-07-01 14:14:28 +0700

    Update controller 2
    Signed-off-by: tienvybui05 <tienvy27052005@gmail.com>

--- src/main/java/uth/edu/dieutrihiemmuon/controllers/Customer/DoctorScheduleController.java
+++ src/main/java/uth/edu/dieutrihiemmuon/controllers/Customer/DoctorScheduleController.java
@@ -1,14 +1,15 @@
 package uth.edu.dieutrihiemmuon.controllers.Customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import uth.edu.dieutrihiemmuon.dto.TreatmentSessionDoctorDTO;
+import uth.edu.dieutrihiemmuon.dto.WorkscheduledoctorDTO;
+import uth.edu.dieutrihiemmuon.models.User;
 import uth.edu.dieutrihiemmuon.services.ICustomerService;
+import uth.edu.dieutrihiemmuon.services.ITreatmentCycleService;
 import uth.edu.dieutrihiemmuon.services.ITreatmentSessionService;
 
 import java.util.List;
@@ -19,6 +20,9 @@ public class DoctorScheduleController {
     private ICustomerService customerService;
     @Autowired
     private ITreatmentSessionService treatmentSessionService;
+
+    @Autowired
+    private ITreatmentCycleService treatmentCycleService;
 
     @GetMapping("/treatmentcycledoctor/{id}")
     public String treatmentcycledoctor(@PathVariable long id, Model model) {
@@ -35,6 +39,22 @@ public class DoctorScheduleController {
         treatmentSessionService.updateTreatmentSessionDTO(treatmentSessionDoctorDTO);
 
         return "redirect:/treatmentcycledoctor/" + treatmentSessionDoctorDTO.getIdTreatmentCycle();
+
+    }
+
+    @GetMapping("/workscheduledoctor")
+    public String workscheduledoctor(Model model, Authentication authentication ) {
+        String username = authentication.getName();
+        User user = customerService.findByUsername(username);
+        long idDoctor = user.getDoctor().getIdDoctor();
+        List<WorkscheduledoctorDTO> wsd = treatmentCycleService.getWorkscheduledoctor(idDoctor);
+        model.addAttribute("scheduleList", wsd);
+        return "customer/doctor/workschedule";
+    }
+    @PostMapping("/saveGeneralNotes")
+    public String saveGeneralNotes(@RequestParam Long id, @RequestParam String note) {
+        treatmentCycleService.updateGeneralNotes(id, note);
+        return "redirect:/workscheduledoctor";
 
     }
 

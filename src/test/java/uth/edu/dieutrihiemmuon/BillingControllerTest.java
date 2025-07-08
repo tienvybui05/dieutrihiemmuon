8d2e3eb4d4d9f5435bfb25d2d479c4d8a8a83781
tienvybui05 <tienvy27052005@gmail.com>
2025-07-08 22:58:10 +0700

    Unit test BillingControllerTest.java
    Signed-off-by: tienvybui05 <tienvy27052005@gmail.com>

---
+++ src/test/java/uth/edu/dieutrihiemmuon/BillingControllerTest.java
@@ -0,0 +1,28 @@
+package uth.edu.dieutrihiemmuon;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uth.edu.dieutrihiemmuon.controllers.Customer.BillingController;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class BillingControllerTest {
+
+    @InjectMocks
+    private BillingController billingController;
+
+    @Test
+    void testHistory_ReturnsHistoryView() {
+        String view = billingController.history();
+        assertEquals("customer/history", view);
+    }
+
+    @Test
+    void testPayment_ReturnsPaymentView() {
+        String view = billingController.payment();
+        assertEquals("customer/payment", view);
+    }
+}
